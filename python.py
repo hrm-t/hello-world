af4b22bf78fa4cdcc7bf202e1b56a565af25aaac
hrm-t <79389105+hrm-t@users.noreply.github.com>
2024-03-04 09:53:48 +0900

    add  python system info

--- python.py
+++ python.py
@@ -1,1 +1,36 @@
 print("Hello World")
+
+
+
+
+# display all system info
+
+import sys
+print(sys.version)
+print(sys.executable)
+print(sys.platform)
+print(sys.argv)
+print(sys.path)
+print(sys.modules)
+print(sys.getrefcount(sys))
+print(sys.getrecursionlimit())
+print(sys.getsizeof(sys))
+print(sys.getdefaultencoding())
+print(sys.getfilesystemencoding())
+print(sys.getwindowsversion())
+print(sys.getwindowsversion().major)
+print(sys.getwindowsversion().minor)
+print(sys.getwindowsversion().build)
+print(sys.getwindowsversion().platform)
+print(sys.getwindowsversion().service_pack)
+print(sys.getwindowsversion().release)
+print(sys.getwindowsversion().version)
+print(sys.getwindowsversion().product_type)
+print(sys.getwindowsversion().product_name)
+print(sys.getwindowsversion().current_build)
+print(sys.getwindowsversion().current_platform)
+print(sys.getwindowsversion().current_service_pack)
+print(sys.getwindowsversion().current_release)
+print(sys.getwindowsversion().current_version)
+print(sys.getwindowsversion().current_product_type)
+print(sys.getwindowsversion().current_product_name)
